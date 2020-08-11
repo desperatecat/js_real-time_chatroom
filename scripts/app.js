96ad9057c8c4a314f07cb33c5747dcbfc01239a9
desperatecat <376198935@qq.com>
2020-08-12 05:59:53 +0800

    update app

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
+//DOM queries
+const chatList = document.querySelector('.chat-list');
+
 //class instances
+const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'Oscar');
 
-//get chats and render
-chatroom.getChats(data => {
-    console.log(data);
-  });
+//get chats & render
+chatroom.getChats(data => chatUI.render(data));
