722fb67c99e8b05a56e6ff1581c048512eee26a0
desperatecat <376198935@qq.com>
2020-08-13 07:52:13 +0800

    update name

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,30 @@
 //DOM queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
-
+const newNameForm = document.querySelector('.new-name');
 
 //add a new chat
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
-
     const message = newChatForm.message.value.trim();
     chatroom.addChat(message)
         .then(() => newChatForm.reset())
         .catch(err => console.log(err));
 });
 
+//update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
 
+    //update name via chatroom
+    const newName = newChatForm.name.value.trim();
+    chatroom.updateName(newName);
+
+    //reset the form
+    newNameForm.reset();
+
+    
+});
 
 //class instances
 const chatUI = new ChatUI(chatList);
